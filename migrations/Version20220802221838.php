e7e85889dc97660a1017349c02218cfddce3a63b
Clement <clm.roig@gmail.com>
2022-08-02 23:54:42 +0200

    add sigil spell entity & routes

---
+++ migrations/Version20220802221838.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220802221838 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE sigil_spell_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE sigil_spell (id INT NOT NULL, target VARCHAR(255) NOT NULL, short_target VARCHAR(255) DEFAULT NULL, content VARCHAR(1500) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE sigil_spell_id_seq CASCADE');
+        $this->addSql('DROP TABLE sigil_spell');
+    }
+}
---
+++ src/Entity/SigilSpell.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\SigilSpellRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: SigilSpellRepository::class)]
+#[ApiResource(shortName: 'sigil-spells')]
+class SigilSpell
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private ?string $target = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $short_target = null;
+
+    #[ORM\Column(length: 1500)]
+    #[Assert\NotBlank]
+    private ?string $content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTarget(): ?string
+    {
+        return $this->target;
+    }
+
+    public function setTarget(string $target): self
+    {
+        $this->target = $target;
+
+        return $this;
+    }
+
+    public function getShortTarget(): ?string
+    {
+        return $this->short_target;
+    }
+
+    public function setShortTarget(?string $short_target): self
+    {
+        $this->short_target = $short_target;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
